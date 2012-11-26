c62a9e7f7c097c748ffdc77ea99650fdbb4dce5b
Hiroki Noda <kubo39@gmail.com>
2012-11-27 07:43:40 +0900

    update

--- lib/bossan.rb
+++ lib/bossan.rb
@@ -1,5 +1,6 @@
 require_relative "bossan/version"
 require_relative "bossan/bossan_ext"
+require_relative "rack/handler/bossan"
 
 def run(host='127.0.0.1', port=8000, app)
   Bossan.run(host, port, app)
