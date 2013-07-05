699fc2cd3b4e89af72996c9b3932a9de50b05eb7
Hiroki Noda <kubo39@gmail.com>
2013-07-06 01:32:57 +0900

    add reason phrase to status_code

--- ext/bossan/bossan_ext.c
+++ ext/bossan/bossan_ext.c
@@ -1140,7 +1140,6 @@ set_path(VALUE env, char *buf, int len)
     *t++ = c;
     len--;
   }
-  //*t = 0;
   slen = t - s0;
   slen = urldecode(s0, slen);
 
@@ -1629,7 +1628,6 @@ setup_sock(int fd)
 #else
   r = fcntl(fd, F_SETFL, O_NONBLOCK);
 #endif
-  /* r = fcntl(fd, F_SETFL, O_NONBLOCK); */
   /* assert(r == 0); */
   return r;
 }
@@ -1662,9 +1660,7 @@ new_client_t(int client_fd, char *remote_addr, uint32_t remote_port)
   client->fd = client_fd;
   client->remote_addr = remote_addr;
   client->remote_port = remote_port;
-  /* client->req = new_request(); */
   client->request_queue = new_request_queue();
-  /* client->body_type = BODY_TYPE_NONE; */
   return client;
 }
 
@@ -1696,7 +1692,6 @@ clean_client(client_t *client)
 
   DEBUG("status_code:%d env:%p", client->status_code, req->environ);
   if (req->body) {
-    /* rb_p(req->body); */
     /* free_buffer(req->body); */
     req->body = NULL;
   }
@@ -1768,6 +1763,90 @@ static void init_main_loop(void)
   }
 }
 
+static char*
+get_reason_phrase(int status_code)
+{
+  if (status_code == 200) {
+    return "OK";
+  } else if (status_code == 201) {
+    return "Created";
+  } else if (status_code == 202) {
+    return "Accepted";
+  } else if (status_code == 203) {
+    return "Non-Authoritative Information";
+  } else if (status_code == 204) {
+    return "No Content";
+  } else if (status_code == 205) {
+    return "Reset Content";
+  } else if (status_code == 206) {
+    return "Partial Content";
+  } else if (status_code == 300) {
+    return "Multiple Choices";
+  } else if (status_code == 301) {
+     return "Moved Permanently";
+  } else if (status_code == 302) {
+     return "Found";
+  } else if (status_code == 303) {
+     return "See Other";
+  } else if (status_code == 304) {
+     return "Not Modified";
+  } else if (status_code == 305) {
+     return "Use Proxy";
+  } else if (status_code == 307) {
+     "Temporary Redirect";
+  } else if (status_code == 400) {
+     return "Bad Request";
+  } else if (status_code == 401) {
+     return "Unauthorized";
+  } else if (status_code == 402) {
+     return "Payment Required";
+  } else if (status_code == 403) {
+    return "Forbidden";
+  } else if (status_code == 404) {
+    return "Not Found";
+  } else if (status_code == 405) {
+    return "Method Not Allowed";
+  } else if (status_code == 406) {
+    return "Not Acceptable";
+  } else if (status_code == 407) {
+    return "Proxy Authentication Required";
+  } else if (status_code == 408) {
+    return "Request Time-out";
+  } else if (status_code == 409) {
+    return "Conflict";
+  } else if (status_code == 410) {
+    return "Gone";
+  } else if (status_code == 411) {
+    return "Length Required";
+  } else if (status_code == 412) {
+    return "Precondition Failed";
+  } else if (status_code == 413) {
+    return " Request Entity Too Large";
+  } else if (status_code == 414) {
+    return "Request-URI Too Large";
+  } else if (status_code == 415) {
+    return "Unsupported Media Type";
+  } else if (status_code == 416) {
+    return "Requested range not satisfiable";
+  } else if (status_code == 417) {
+    return "Expectation Failed";
+  } else if (status_code == 500) {
+    return "Internal Server Error";
+  } else if (status_code == 501) {
+    return "Not Implemented";
+  } else if (status_code == 502) {
+    return "Bad Gateway";
+  } else if (status_code == 503) {
+    return "Service Unavailable";
+  } else if (status_code == 504) {
+    return "Gateway Time-out";
+  } else if (status_code == 505) {
+    return "HTTP Version not supported";
+  } else {
+    return "Unknown";
+  }
+}
+
 
 static int
 process_rack_app(client_t *cli)
@@ -1793,8 +1872,6 @@ process_rack_app(client_t *cli)
   
   VALUE* response_as_arr = RARRAY_PTR(response_arr);
 
-  /* rb_p(cli->response); */
-
   if (TYPE(response_as_arr[0]) != T_FIXNUM ||
       TYPE(response_as_arr[1]) != T_HASH) {
     return 0;
@@ -1813,8 +1890,11 @@ process_rack_app(client_t *cli)
   errno = 0;
   /* printf("status code: %d\n", cli->status_code); */
 
+  char* reason_phrase;
+  reason_phrase = get_reason_phrase(cli->status_code);
+
   char buff[256];
-  sprintf(buff, "HTTP/1.%d %d\r\n", cli->http_parser->http_minor, cli->status_code); // TODO: add reasons
+  sprintf(buff, "HTTP/1.%d %d %s\r\n", cli->http_parser->http_minor, cli->status_code, reason_phrase);
   cli->http_status = rb_str_new(buff, strlen(buff));
 
   //check response
