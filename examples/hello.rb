0418afb4b5b90a1bda729bdbdbd2b5ba9a1f23c3
Hiroki Noda <kubo39@gmail.com>
2013-07-17 07:37:26 +0900

    minor fix

--- examples/hello.rb
+++ examples/hello.rb
@@ -2,13 +2,13 @@ require_relative '../lib/bossan'
 
 Bossan.listen('127.0.0.1', 8000)
 Bossan.run(proc {|env|
-            body = 'hello, world!'
-            [
-             200,          # Status code
-             {             # Response headers
-               'Content-Type' => 'text/html',
-               'Content-Length' => body.size.to_s,
-             },
-             [body]        # Response body
-            ]
-          })
+             body = ['hello, world!']
+             [
+              200,          # Status code
+              {             # Response headers
+                'Content-Type' => 'text/html',
+                'Content-Length' => body.join.size.to_s,
+              },
+              body        # Response body
+             ]
+           })
